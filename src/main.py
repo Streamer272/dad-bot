cb315adb506dd93cf84fe7b5e121f87b8855b61c
Daniel Svitan <dankosaurus9@gmail.com>
2021-06-11 22:09:40 +0200

    :sparkles: Adds basic command performing

--- src/main.py
+++ src/main.py
@@ -17,16 +17,19 @@ class CustomClient(discord.Client):
             return None
 
         try:
+            # command perform
+            if message.content.startswith(DatabaseController.get_value(message.guild.name, "command_prefix"), 0, 2):
+                await self.perform_command(message)
+                return None
+
+            # is disabled check
             if DatabaseController.get_status(message.guild.name):
                 return None
 
         except TypeError:
             DatabaseController.create_record(message.guild.name, [])
 
-        if message.content.startswith(DatabaseController.get_value(message.guild.name, "command_prefix"), 0, 2):
-            await self.perform_command(message)
-            return None
-
+        # sending back message
         for i in loads(DatabaseController.get_value(message.guild.name, "im_variations")):
             if message.content.lower().startswith(i.lower() + " ", 0):
                 await message.channel.send(
@@ -34,23 +37,21 @@ class CustomClient(discord.Client):
                         message.content.replace(message.content[0:len(i)] + " ", ""))
                 )
 
-    # TODO: rewrite this function
     async def perform_command(self, message):
-        # if message.content.startswith(self.command_symbol + "disable"):
-        #     config = ConfigController.get_config(message.guild.name)
-        #     config["disabled"] = True
-        #     ConfigController.set_config(message.guild.name, config)
-        #
-        # elif message.content.startswith(self.command_symbol + "enable"):
-        #     config = ConfigController.get_config(message.guild.name)
-        #     config["disabled"] = False
-        #     ConfigController.set_config(message.guild.name, config)
-        #
-        # elif message.content.startswith(self.command_symbol + "set-message "):
-        #     config = ConfigController.get_config(message.guild.name)
-        #     config["message"] = message.content.replace(self.command_symbol + "set-message ", "")
-        #     ConfigController.set_config(message.guild.name, config)
-        pass
+        # we don't want command prefix here
+        message.content = message.content[1:]
+
+        if message.content == "help":
+            await message.channel.send("""```
+Available commands:
+    help - displays help message
+    set-status <true/false> - sets bot status
+```""")
+
+        elif message.content.startswith("set-status"):
+            DatabaseController.set_status(message.guild.name, "f" in message.content.replace("set-status ", ""))
+
+        # TODO: finish this function
 
     async def on_error(self, event, *args, **kwargs):
         error_message = f"Error occurred while handling {event} with {args}, {kwargs}"
