e2635c2d2145f6e23c896c60421473e62e475ba4
Daniel Svitan <dankosaurus9@gmail.com>
2021-06-13 12:57:13 +0200

    :sparkles: Adds add-log-dir-if-not-exists feature

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from os import mkdir, path
 
 
 # TODO idea: maybe try module logging for this?
@@ -7,6 +8,9 @@ class Logger:
     def log(message, console=True):
         if console:
             print(f"{datetime.now()}: {message}")
+
+        if not path.exists("log"):
+            mkdir("log")
 
         file = open(f"./log/log-{str(date.today())}.txt", "a")
         file.write(f"{datetime.now()}: {message}\n")
