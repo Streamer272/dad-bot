88d457d80847ae1a3fef0187550669c8ba76cce8
Daniel Svitan <dankosaurus9@gmail.com>
2021-06-13 13:05:10 +0200

    :bug: Fixes seed.py

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
 from ss3dbc import *
 
-db = Database("../db/database.sql")
+db = Database("./db/database.sql")
 db.create_table("Server", "server_name TEXT, command_prefix TEXT, im_variations TEXT, message TEXT, disabled BOOLEAN")
 db.close()
